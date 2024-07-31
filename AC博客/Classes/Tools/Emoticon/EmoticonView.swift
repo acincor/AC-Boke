2fa8058ea7a7ceb58b4a6b735f1e695932b486a9
Mhc-inc <Wf6350177@163.com>
2024-07-31 14:49:17 +0800

    取消表情包toolBar机制

--- AC博客/Classes/Tools/Emoticon/EmoticonView.swift
+++ AC博客/Classes/Tools/Emoticon/EmoticonView.swift
@@ -18,10 +18,6 @@ class EmoticonView: UIView {
         super.init(frame: rect)
         backgroundColor = .systemBackground
         setupUI()
-        let indexPath = IndexPath(item: 0, section: 1)
-        DispatchQueue.main.async {
-            self.collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
-        }
     }
     private class EmoticonViewCell: UICollectionViewCell {
         override init(frame: CGRect) {
@@ -37,9 +33,6 @@ class EmoticonView: UIView {
             didSet {
                 emoticonButton.setImage(UIImage(contentsOfFile: emoticon!.imagePath), for: .normal)
                 emoticonButton.setTitle(emoticon?.emoji, for: .normal)
-                if emoticon!.isRemoved {
-                    emoticonButton.setImage(UIImage(named: "compose_emotion_delete"), for: .normal)
-                }
             }
         }
         required init?(coder: NSCoder) {
@@ -50,70 +43,19 @@ class EmoticonView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    private lazy var collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: EmoticonLayout())
-    private lazy var toolbar = UIToolbar()
-    private class EmoticonLayout: UICollectionViewFlowLayout {
-        override func prepare() {
-            super.prepare()
-            let col: CGFloat = 7
-            let row: CGFloat = 3
-                let deviceType = UIDevice.current.userInterfaceIdiom
-            let w = collectionView!.bounds.width / col
-            var margin: CGFloat
-            switch deviceType {
-            case .pad:
-                let h = collectionView!.bounds.height / row
-                margin  = CGFloat(Int((collectionView!.bounds.height - row * h) * 0.5))
-                itemSize = CGSize(width: w, height: h)
-            default:
-                margin  = CGFloat(Int((collectionView!.bounds.height - row * w) * 0.5))
-                itemSize = CGSize(width: w, height: w)
-            }
-            sectionInset = UIEdgeInsets(top: margin, left: 0, bottom: margin, right: 0)
-            minimumInteritemSpacing = 0
-            minimumLineSpacing = 0
-            scrollDirection = .horizontal
-            collectionView?.isPagingEnabled = true
-            collectionView?.bounces = false
-        }
-    }
+    private lazy var collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
 }
 private extension EmoticonView {
     func setupUI() {
         backgroundColor = .systemBackground
         addSubview(collectionView)
-        addSubview(toolbar)
-        toolbar.snp.makeConstraints { make in
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
             make.bottom.equalTo(self.snp.bottom)
             make.left.equalTo(self.snp.left)
             make.right.equalTo(self.snp.right)
-            make.height.equalTo(36)
         }
-        collectionView.snp.makeConstraints { make in
-            make.top.equalTo(self.snp.top)
-            make.bottom.equalTo(toolbar.snp.bottom)
-            make.left.equalTo(self.snp.left)
-            make.right.equalTo(self.snp.right)
-        }
-        prepareToolbar()
         prepareCollectionView()
-    }
-    func prepareToolbar() {
-        tintColor = .darkGray
-        var items = [UIBarButtonItem]()
-        var index = 0
-        for p in packages {
-            items.append(UIBarButtonItem(title: NSLocalizedString(p.group_name_cn ?? "", comment: ""), style: .plain, target: self, action: #selector(EmoticonView.clickItem(item:))))
-            items.last?.tag = index
-            index += 1
-            items.append(UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil))
-        }
-        items.removeLast()
-        toolbar.items = items
-    }
-    @objc func clickItem(item: UIBarButtonItem) {
-        let indexPath = IndexPath(item: 0, section: item.tag)
-        collectionView.scrollToItem(at: indexPath, at: .left, animated: true)
     }
     func prepareCollectionView() {
         collectionView.backgroundColor = UIColor.systemBackground
