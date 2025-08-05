1650847ff52992e11450f67968f1fb5227ca5889
acincor <Wf6350177@163.com>
2025-08-05 20:14:58 +0800

    彻底去除SDWebImage。修改了NetworkTools部分的屎山代码

---
+++ Acblog/Classes/Tools/Extension/UIImage+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIImage+Extension.swift
+//  Acblog
+//
+//  Created by acincor on 2025/8/5.
+//
+
+import UIKit
+import ImageIO
+import MobileCoreServices
+import UniformTypeIdentifiers
+// MARK: - Image format
+extension UIImage {
+    func gifData(duration: TimeInterval, repeat count: Int) -> Data? {
+        guard let images = self.images else {
+            return nil
+        }
+     
+        let frameCount = images.count
+        let gifDuration = duration <= 0.0 ? self.duration / Double(frameCount) : duration / Double(frameCount)
+     
+        let frameProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: gifDuration]]
+        let imageProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: count]]
+     
+        let data = NSMutableData()
+     
+        guard let destination = CGImageDestinationCreateWithData(data, UTType.gif.identifier as CFString, frameCount, nil) else {
+            return nil
+        }
+        CGImageDestinationSetProperties(destination, imageProperties as CFDictionary)
+     
+        for image in images {
+            CGImageDestinationAddImage(destination, image.cgImage!, frameProperties as CFDictionary)
+        }
+     
+        return CGImageDestinationFinalize(destination) ? Data(data) : nil
+    }
+    func gifData() -> Data? {
+        return self.gifData(duration: 0.0, repeat: 0)
+    }
+}
